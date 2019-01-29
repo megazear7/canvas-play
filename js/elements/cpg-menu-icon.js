8be518fd7899ee8daa6b8133cbe77f2625c90eba
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2019-01-29 11:00:17 -0500

    Got the animation loop running within the class

--- js/elements/cpg-menu-icon.js
+++ js/elements/cpg-menu-icon.js
@@ -10,10 +10,28 @@ export default class CpgMenuIcon extends HTMLElement {
       </style>
       <canvas></canvas>
     `;
-    this.canvas = shadow.querySelector('canvas');
 
+    this.canvas = shadow.querySelector('canvas');
     this.canvas.width = 50;
     this.canvas.height = 50;
+    this.context = this.canvas.getContext('2d');
+    this.mousePosition = {x: 0, y: 0};
+
+    this.canvas.addEventListener('mousemove', event => {
+      this.mousePosition = getMousePos(this.canvas, event);
+    }, false);
+
+    var animate;
+    animate = () => {
+      requestAnimationFrame(animate);
+      this.doAnimate();
+    }
+    animate();
+  }
+
+  doAnimate() {
+    this.context.clearRect(0, 0, 50, 50);
+    this.context.fillRect(0, 0, 30, 30);
   }
 }
 
