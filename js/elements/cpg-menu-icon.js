d72bbb50d136c3261add4daf036433ec512d0e9b
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2019-01-29 11:31:16 -0500

    Added hover state

--- js/elements/cpg-menu-icon.js
+++ js/elements/cpg-menu-icon.js
@@ -6,6 +6,10 @@ export default class CpgMenuIcon extends HTMLElement {
     var shadow = this.attachShadow({mode: 'open'});
     shadow.innerHTML = `
       <style media="screen">
+        :host {
+          cursor: pointer;
+        }
+
         canvas {
           padding: 0;
           margin: 0;
@@ -17,15 +21,18 @@ export default class CpgMenuIcon extends HTMLElement {
     this.canvas = shadow.querySelector('canvas');
     this.context = this.canvas.getContext('2d');
     this.mousePosition = {x: 0, y: 0};
-
     this.width = this.clientWidth;
     this.height = this.clientHeight;
     this.canvas.width = this.width;
     this.canvas.height = this.height;
+    this.hovering = false;
 
     this.canvas.addEventListener('mousemove', event => {
       this.mousePosition = getMousePos(this.canvas, event);
     }, false);
+
+    this.canvas.addEventListener("mouseenter", event => this.hovering = true);
+    this.canvas.addEventListener("mouseleave", event => this.hovering = false);
 
     var animate;
     animate = () => {
@@ -36,6 +43,19 @@ export default class CpgMenuIcon extends HTMLElement {
   }
 
   doAnimate() {
+    this.update();
+    this.draw();
+  }
+
+  update() {
+    if (this.hovering) {
+      // move to arrow state
+    } else {
+      // move back to rest
+    }
+  }
+
+  draw() {
     this.context.clearRect(0, 0, this.width, this.height);
     this.drawLine(this.offsetWidth * (1/8), 2);
     this.drawLine(this.offsetWidth * (4/8), 2);
