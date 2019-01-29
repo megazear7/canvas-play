9e067bec428a672b98aac262629235524b08a7e1
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2019-01-29 05:08:50 -0500

    Added class to represent cracks in the granite

--- js/granite-crumble.js
+++ js/granite-crumble.js
@@ -4,16 +4,54 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
 
-var segmentCount = 2;
-var lineCount = 5;
+class Crack {
+  constructor({segmentCount = 2} = {}) {
+    this.points = [];
 
-for (var j=0; j < lineCount; j++) {
-  c.beginPath();
-  for (var i=0; i < segmentCount+1; i++) {
-    c.lineTo(randomX(), randomY());
+    for (var i=0; i < segmentCount+1; i++) {
+      this.points.push({
+        x: randomX(),
+        y: randomY()
+      });
+    }
   }
-  c.strokeStyle = 'rgba(255, 0, 0, 0.5)';
-  c.stroke();
+
+  draw() {
+    c.beginPath();
+    this.points.forEach(point => c.lineTo(point.x, point.y));
+    c.strokeStyle = 'rgba(255, 0, 0, 0.5)';
+    c.stroke();
+  }
+
+  update() {
+    this.draw();
+  }
+}
+
+
+var cracks = [];
+var crackCount = randomNumber({min: 4, max: 7});
+
+for (var i = 0; i < crackCount; i++) {
+  cracks.push(new Crack({segmentCount: randomSegmentCount()}));
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, window.innerWidth, window.innerHeight);
+  cracks.forEach(crack => crack.update());
+}
+
+animate();
+
+function randomNumber({min = 1, max = 100}) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomSegmentCount(segmentCount = 3) {
+  return Math.ceil(Math.random() * segmentCount);
 }
 
 function randomY() {
