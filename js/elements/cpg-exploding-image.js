5fa80cfcc7744e12af2f5a6d4ababdde178786a7
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2019-01-31 20:05:43 -0500

    Rendering the particles

--- js/elements/cpg-exploding-image.js
+++ js/elements/cpg-exploding-image.js
@@ -20,6 +20,7 @@ export default class CpgExplodingImage extends HTMLElement {
     this.canvas = this.shadow.querySelector('canvas');
     this.context = this.canvas.getContext('2d');
     this.mousePosition = {x: 0, y: 0};
+    this.particles = [];
 
     /* ---------------------- */
     /* Configuration Options */
@@ -50,6 +51,34 @@ export default class CpgExplodingImage extends HTMLElement {
     this.canvas.height = this.png.height * 3;
     this.context.drawImage(this.png, 0, 0);
 
+    var data = this.context.getImageData(0, 0, this.png.width, this.png.height);
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    for (var y = 0, y2 = data.height; y < y2; y++) {
+      for (var x = 0, x2 = data.width; x < x2; x++) {
+        var p = y * 4 * data.width + x * 4;
+        if (data.data[p + 3] > 129) {
+          var particle = {
+            x: this.png.width / 2,
+            y: this.png.height / 2,
+            target: {
+              x: x,
+              y: y,
+            },
+            speed: Math.random() * 4 + 2,
+            color:
+              "rgb(" +
+              data.data[p] +
+              "," +
+              data.data[p + 1] +
+              "," +
+              data.data[p + 2] +
+              ")"
+          };
+          this.particles.push(particle);
+        }
+      }
+    }
+
     var animate;
     animate = () => {
       requestAnimationFrame(animate);
@@ -67,7 +96,11 @@ export default class CpgExplodingImage extends HTMLElement {
   }
 
   draw() {
-    //this.context.clearRect(0, 0, this.width, this.height);
+    this.context.clearRect(0, 0, this.width, this.height);
+    this.particles.forEach(particle => {
+      this.context.fillStyle = particle.color;
+      this.context.fillRect(particle.x * 3, particle.y * 3, 3, 3);
+    });
   }
 }
 
