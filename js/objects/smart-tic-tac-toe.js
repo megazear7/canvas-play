3e4b01d1c49dea68e163c96b525de75e7f938981
Alex Lockhart <alexlockahrt7@gmail.com>
2020-10-28 05:20:57 -0400

    Cleaned up tic tac toe

--- js/objects/smart-tic-tac-toe.js
+++ js/objects/smart-tic-tac-toe.js
@@ -6,47 +6,42 @@ export default class StaticImage {
               x,
               y,
               size,
+              lineColor = "rgba(100,123,212,1)",
+              lineThickness = 10,
             } = {}) {
     this.context = context;
     this.x = x;
     this.y = y;
     this.size = size;
+    this.lineColor = lineColor;
+    this.lineThickness = lineThickness;
 
     // Set starting point
     this.boardState = [ 0, 0, 0, 0, 0, 0, 0, 0, 0 ];
+    this.lines = this.makeLines();
   }
 
   draw() {
-    console.log('test', this.x, this.y, this.size);
-
-    let lines = [];
-
-    lines.push({
-      p1: { x: this.x + (this.size * (1/3)), y: this.y },
-      p2: { x: this.x + (this.size * (1/3)), y: this.y + this.size },
-    });
-
-    lines.push({
-      p1: { x: this.x + (this.size * (2/3)), y: this.y },
-      p2: { x: this.x + (this.size * (2/3)), y: this.y + this.size },
-    });
-
-    lines.push({
-      p1: { x: this.x,             y: this.y + (this.size * (1/3)) },
-      p2: { x: this.x + this.size, y: this.y + (this.size * (1/3)) },
-    });
-
-    lines.push({
-      p1: { x: this.x,             y: this.y + (this.size * (2/3)) },
-      p2: { x: this.x + this.size, y: this.y + (this.size * (2/3)) },
-    });
-
-    lines.forEach(line => {
-      drawLine(this.context, line.p1, line.p2, 9, "rgba(100,123,212,1)");
-    });
+    this.lines.forEach(line => drawLine(this.context, line.p1, line.p2, this.lineThickness, this.lineColor));
   }
 
   update() {
     this.draw();
+  }
+
+  makeLines() {
+    return [{
+        p1: { x: this.x + (this.size * (1/3)), y: this.y },
+        p2: { x: this.x + (this.size * (1/3)), y: this.y + this.size },
+      }, {
+        p1: { x: this.x + (this.size * (2/3)), y: this.y },
+        p2: { x: this.x + (this.size * (2/3)), y: this.y + this.size },
+      }, {
+        p1: { x: this.x,             y: this.y + (this.size * (1/3)) },
+        p2: { x: this.x + this.size, y: this.y + (this.size * (1/3)) },
+      }, {
+        p1: { x: this.x,             y: this.y + (this.size * (2/3)) },
+        p2: { x: this.x + this.size, y: this.y + (this.size * (2/3)) },
+    }];
   }
 }
