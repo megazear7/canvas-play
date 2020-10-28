4ae5dc29d3618a1e5445c1ced5d82d30120f5196
Alex Lockhart <alexlockahrt7@gmail.com>
2020-10-28 05:41:10 -0400

    Drawing lines now

--- js/objects/smart-tic-tac-toe.js
+++ js/objects/smart-tic-tac-toe.js
@@ -6,27 +6,60 @@ export default class StaticImage {
               x,
               y,
               size,
-              lineColor = "rgba(100,123,212,1)",
+              color = "rgba(100,123,212,1)",
               lineThickness = 10,
             } = {}) {
     this.context = context;
     this.x = x;
     this.y = y;
     this.size = size;
-    this.lineColor = lineColor;
+    this.color = color;
     this.lineThickness = lineThickness;
 
     // Set starting point
-    this.boardState = [ 0, 0, 0, 0, 0, 0, 0, 0, 0 ];
+    this.cells = [ 1, 2, 0, 0, 0, 0, 0, 0, 0 ];
     this.lines = this.makeLines();
   }
 
   draw() {
-    this.lines.forEach(line => drawLine(this.context, line.p1, line.p2, this.lineThickness, this.lineColor));
+    this.lines.forEach(line => drawLine(this.context, line.p1, line.p2, this.lineThickness, this.color));
+
+    this.cells.forEach((cell, index) => {
+      if (cell === 1) {
+        this.drawX(index);
+      } else if (cell === 2) {
+        this.drawY(index);
+      }
+    });
   }
 
   update() {
     this.draw();
+  }
+
+  drawX(pos) {
+    const topLeft = this.getPointForPos(pos);
+
+    const xLines = [{
+      p1: { x: topLeft.x + (this.lineThickness*2), y: topLeft.y + (this.lineThickness*2) },
+      p2: { x: topLeft.x + (this.size * (1/3)) - (this.lineThickness*2), y: topLeft.y + (this.size * (1/3)) - (this.lineThickness*2)},
+    }, {
+      p1: { x: topLeft.x + (this.size * (1/3)) - (this.lineThickness*2), y: topLeft.y + (this.lineThickness*2) },
+      p2: { x: topLeft.x + (this.lineThickness*2), y: topLeft.y + (this.size * (1/3)) - (this.lineThickness*2) },
+    }];
+
+    xLines.forEach(line => drawLine(this.context, line.p1, line.p2, this.lineThickness, this.color));
+  }
+
+  drawY(pos) {
+    const topLeft = this.getPointForPos(pos);
+  }
+
+  getPointForPos(pos) {
+    const gridX = (pos % 3) + 1;
+    const gridY = Math.ceil(pos/3);
+
+    return { x: this.x + (this.size * (gridX/3)), y: this.y + (this.size * (gridY/3)) };
   }
 
   makeLines() {
